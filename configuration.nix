b5a1157b3882993f37ca77c3b9b45c3a46caaa6a
¨Christian <C.Wittpahl@live.de>
2024-03-05 00:07:03 +0100

    23 current  2024-03-05 00:06:57  23.11.4761.5bf1cadb72ab  6.1.79                          *

--- configuration.nix
+++ configuration.nix
@@ -142,6 +142,15 @@
   #  wget
   ];
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+    ];
+  };
+
+
 
   # Enable OpenGL
   hardware.opengl = {
